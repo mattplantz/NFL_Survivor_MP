3c326708eb5c81e9411037958bcd4ed5515e205e
mattplantz <118403738+mattplantz@users.noreply.github.com>
2023-08-22 11:24:46 -0500

    Update main.py
    options jt

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import streamlit as st
 
+nfl_teams = ['Arizona Cardinals','Atlanta Falcons','Baltimore Ravens','Buffalo Bills','Carolina Panthers','Chicago Bears','Cincinnati Bengals','Cleveland Browns','Dallas Cowboys','Denver Broncos','Detroit Lions','Green Bay Packers','Houston Texans','Indianapolis Colts','Jacksonville Jaguars','Kansas City Chiefs','Las Vegas Raiders','Los Angeles Chargers','Los Angeles Rams','Miami Dolphins','Minnesota Vikings','New England Patriots','New Orleans Saints','New York Giants','New York Jets','Philadelphia Eagles','Pittsburgh Steelers','San Francisco 49ers','Seattle Seahawks','Tampa Bay Buccaneers','Tennessee Titans','Washington Commanders']
 @st.cache_data(ttl=600)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
 df = load_data(st.secrets["public_gsheets_url"])
-edited_df = st.data_editor(df)
+edited_df = st.data_editor(df,
+                          column_config = {
+                          "Week 1": st.column_config.SelectboxColumn("Week 1", options = nfl_teams),
+                          })
