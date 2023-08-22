7d3499017d59f0e8cac9b40d30d5d988a6fe8db0
mattplantz <118403738+mattplantz@users.noreply.github.com>
2023-08-22 10:55:02 -0500

    Create main.py
    adding base function

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+
+edited_df = st.data_editor(df)
